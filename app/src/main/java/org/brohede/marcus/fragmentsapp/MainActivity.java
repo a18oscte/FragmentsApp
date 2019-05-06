d527e3a1c4df81ae33061015a88e48341dd72ea5
a18oscte <a18oscte@student.his.se>
2019-05-06 14:21:45 +0200

    Lekt lite men det funkar inte

--- app/src/main/java/org/brohede/marcus/fragmentsapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/fragmentsapp/MainActivity.java
@@ -2,20 +2,41 @@ package org.brohede.marcus.fragmentsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
-    private static final String[] mountainLocations = {"Alps","Alps","Alaska"};
-    private static final int[] mountainHeights ={4478,4808,6190};
-    public static final List<Mountain> MOUNTAINS = new ArrayList<Mountain>();
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
 
         /*
         TODO: You should create an app that uses fragments and orientation
